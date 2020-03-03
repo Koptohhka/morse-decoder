ebcc991e94e245446f2c5de5b86a990ea92a1261
Pan Artur <artur.pankov.16@list.ru>
2020-03-03 17:01:17 +0300

    feat: add final solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,42 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const DECODE_MAP = {
+        11: '-',
+        10: '.',
+        '**********': ' '
+    }
+
+    let decodedStr = '';
+    //Алгоритм старт
+    let testArr = [];
+    let strMultiply = str.length / 10;    
+
+    for (let j = 0; j < strMultiply; j++) {
+        let testVarr = j * 10;
+        let secondTestVarr = testVarr + 10;        
+
+        testArr.push([]);
+        testArr[j].push('');
+
+        for (let i = testVarr; i < secondTestVarr; i += 2) {
+            if (DECODE_MAP[(str[i] + str[i + 1])]) {
+                testArr[j] += DECODE_MAP[(str[i] + str[i + 1])];
+            }
+        }
+    }
+
+    testArr.forEach((it) => {
+        if (MORSE_TABLE[it]) {
+            decodedStr += MORSE_TABLE[it];
+        } else {
+            decodedStr += ' ';
+        }
+    })
+
+    //Алгоритм конец
+
+    return decodedStr;
 }
 
 module.exports = {
